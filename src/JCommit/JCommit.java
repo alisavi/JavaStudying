7e53cbb3a0e6ac9d80e29ebef815a6ef8afd2481
alisavi <alisa.victorova@outlook.com>
2025-02-15 15:27:01 +0300

    First Java branch

--- src/JCommit/JCommit.java
+++ src/JCommit/JCommit.java
@@ -4,5 +4,6 @@ public class JCommit {
     public static void main(String[] args) {
         System.out.println("First Java commit");
         System.out.println("Comment for the commit");
+        System.out.println("Java "+ (12 + 9));
     }
 }
