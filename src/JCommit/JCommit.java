e84bd0e48e6ccaea25f8ae7b6ccfc8eb9daa9347
alisavi <alisa.victorova@outlook.com>
2025-02-15 15:22:47 +0300

    First Java commit

---
+++ src/JCommit/JCommit.java
@@ -0,0 +1,8 @@
+package JCommit;
+
+public class JCommit {
+    public static void main(String[] args) {
+        System.out.println("First Java commit");
+        System.out.println("Comment for the commit");
+    }
+}
