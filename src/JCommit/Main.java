42ec6af71718fa7dae3636050df3ec514ca8721c
alisavi <alisa.victorova@outlook.com>
2025-02-16 11:02:55 +0300

    Minor changes

--- src/JCommit/Main.java
+++ src/JCommit/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         JCommit commit = new JCommit();
         commit.print();
+        double epsilon = 0.000001D;
         float a = 0.4F;
         float b = 0.1F;
         float subtracted = a - b;
@@ -12,6 +13,8 @@ public class Main {
             System.out.println("Разница float равно 0,3");
         } else if (subtracted == 0.30000000000000004) {
             System.out.println("Разница float равно 0,30000000000000004"); // float подрезал значение?
+        } else if (Math.abs(subtracted - 0.3) < epsilon) {
+            System.out.println("Разница float равна 0.3");
         }
         double c = 0.4;
         double d = 0.1;
@@ -21,6 +24,9 @@ public class Main {
             System.out.println("Разница равно 0,3");
         } else if (s == 0.30000000000000004) {
             System.out.println("Разница равно 0,30000000000000004");
+        }
+        if (Math.abs(s - 0.3) < epsilon) {
+            System.out.println("Разница равна 0.3");
         }
     }
 }
